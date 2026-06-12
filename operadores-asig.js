34a197500c2d8eea623b66818ae2a21033e0121a
ANGEL777-CR7 <rosasgarciajoseangel@gmail.com>
2026-06-12 08:52:12 -0600

    Asignacion

---
+++ operadores-asig.js
@@ -0,0 +1,22 @@
+//operadores de asignación en JavaScript
+//1. Asignación simple (=): Se utiliza para asignar un valor a una variable.
+let x = 10; // x ahora tiene el valor de 10
+
+//2. Asignación de suma (+=): Se utiliza para sumar un valor a una variable y asignar el resultado a esa variable.
+x += 5; // x ahora es 15 (10 + 5)   
+
+//3. Asignación de resta (-=): Se utiliza para restar un valor de una variable y asignar el resultado a esa variable.
+x -= 3; // x ahora es 12 (15 - 3)   
+
+//4. Asignación de multiplicación (*=): Se utiliza para multiplicar una variable por un valor y asignar el resultado a esa variable.
+x *= 2; // x ahora es 24 (12 * 2)   
+
+//5. Asignación de división (/=): Se utiliza para dividir una variable por un valor y asignar el resultado a esa variable.
+x /= 4; // x ahora es 6 (24 / 4)
+
+//6. Asignación de módulo (%=): Se utiliza para obtener el residuo de una variable dividida por un valor y asignar el resultado a esa variable.
+x %= 5; // x ahora es 1 (6 % 5) 
+
+//7. Contatenación de cadenas (+=): Se utiliza para concatenar una cadena de texto a una variable de tipo string y asignar el resultado a esa variable.
+let saludo = "Hola, ";
+saludo += "mundo!"; // saludo ahora es "Hola, mundo!" (concatenación de "Hola, " y "mundo!")    
