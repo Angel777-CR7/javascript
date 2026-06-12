aa04d8d45b1ecadae43b8d359ceca725da4fa81a
ANGEL777-CR7 <rosasgarciajoseangel@gmail.com>
2026-06-12 08:34:13 -0600

    aritmeticos

---
+++ operadores_arit.js
@@ -0,0 +1,26 @@
+//operadores aritméticos en JavaScript
+//1. Suma (+): Se utiliza para sumar dos números o concatenar cadenas de texto.
+let a = 10;
+let b = 5;
+let suma = a + b; // resultado: 15  
+
+//2. Resta (-): Se utiliza para restar un número de otro.
+let resta = a - b; // resultado: 5
+
+//3. Multiplicación (*): Se utiliza para multiplicar dos números.
+let multiplicacion = a * b; // resultado: 50
+
+//4. División (/): Se utiliza para dividir un número por otro.
+let division = a / b; // resultado: 2
+
+//5. Módulo (%): Se utiliza para obtener el residuo de una división.
+let modulo = a % b; // resultado: 0 
+
+//6. Exponenciación (**): Se utiliza para elevar un número a la potencia de otro.
+let exponente = a ** b; // resultado: 100000    
+
+//7. Incremento (++): Se utiliza para aumentar el valor de una variable en 1.
+a++; // a ahora es 11
+
+//8. Decremento (--): Se utiliza para disminuir el valor de una variable en 1.
+b--; // b ahora es 4
